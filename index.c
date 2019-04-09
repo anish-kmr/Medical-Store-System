f45eff30ac190774fcd58da0d362c71f7113ea5f
Anish <anish12k07@gmail.com>
2019-04-09 19:33:35 +0530

    Update index.c

--- index.c
+++ index.c
@@ -94,7 +94,7 @@ int validate(Credentials* info,int type){
     if(info->username[0]=='\0'|| info->pwd[0]=='\0' || info->email[0]=='\0' || info->cpwd[0]=='\0' || info->address[0]=='\0'  ) return 0;
     if(strcmp(info->pwd,info->cpwd)) return -1;
     if(type == OWNER){
-        if( info->storename[0]=='\0' ) return 0
+        if( info->storename[0]=='\0' ) return 0;
     }
     return status;
 }
